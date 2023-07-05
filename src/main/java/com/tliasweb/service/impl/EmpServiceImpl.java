3f973ec6561aaff7005992322713fcc938f03dc4
Joey <headmastereggy@gmail.com>
2023-07-05 19:03:38 +0800

    init project

--- src/main/java/com/tliasweb/service/impl/EmpServiceImpl.java
+++ src/main/java/com/tliasweb/service/impl/EmpServiceImpl.java
@@ -1,8 +1,10 @@
 package com.tliasweb.service.impl;
 
 import com.tliasweb.service.EmpService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class EmpServiceImpl implements EmpService {
 }
